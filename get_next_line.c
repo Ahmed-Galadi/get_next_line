5eecae937a7c13cef39d4292dd2d22dd61253e58
Ahmed-Galadi <galadiahmed99@gmail.com>
2023-12-01 23:19:30 +0100

    still working on the logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:47:22 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/01 22:30:41 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/01 23:18:47 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,36 @@
 
 int has_newline(char *str)
 {
-	
+	int		i;
+
+	i = 0;
+	while(str[i])
+	{
+		if(str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
 }
 
 char *get_next_line(int fd)
 {
 	static my_list *list_buff;
-	size_t buff_size = 10;
-	int chars_read;
-	if (fd < 0 || buff_size >= 0 || read(fd, list_buff->str, buff_size) < 0)
+	char *output;
+	size_t buff_size;
+	
+	buff_size = 10;
+	if (fd < 0 || buff_size >= 0)
 		return (NULL);
-	while(list_buff->str )
-	return ();
+	
+	while(1)
+	{
+		read(fd, list_buff->str, 10);
+		if(has_newline(list_buff->str))
+		{
+			
+		}
+		list_buff = list_buff->next;
+	}
+	return (output);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:45:30 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/01 21:56:47 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/01 22:55:48 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-char *get_next_line(int fd);
 typedef struct s_list
 {
 	char	*str;
 	struct	s_list *next;
 } my_list;
+char	*get_next_line(int fd);
+my_list	*ft_lstnew(void *content);
+void	ft_lstadd_back(my_list **lst, my_list *new);
+my_list	*ft_lstlast(my_list *lst);
 #endif
