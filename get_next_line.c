af06a3877525301292a098746c51108fd0447985
Ahmed-Galadi <galadiahmed99@gmail.com>
2023-12-03 10:53:03 +0100

    working on a better logic

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:47:22 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/02 17:53:50 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/03 10:51:35 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,38 +29,52 @@ int has_newline(char *str)
 	return (0);
 }
 
-int lenstr(char *str)
-{}
-
-char *joining(char *s1, char *s2)
+int ft_strlen(char *str)
 {
-	int s1_length;
-	int s2_length;
+	int		i;
+
+	i = 0;
+	while(str[i])
+		i++;
+	return (i);
+}
+
+char *ft_strjoin(char *s1, char *s2, int buffsize)
+{
+	static int	size;
+	char		*joined;
+	int			i;
+	int			j;
+
+	size += buffsize;
+	joined = (char *)malloc(size + 1);
+	i = -1;
+	j = -1;
+	while (s1[++i])
+		joined[i] = s1[i];
+	while(s2[j])
+		joined[++i] = s2[++j];
+	joined[i] = '\0';
+	return (joined);
 }
 
 char *get_next_line(int fd)
 {
-	static my_list *list_buff;
 	char *output;
 	int buff_size;
 	buff_size = 10;
-	my_list *curr;
 	char content[11] = "\0";
 	int read_until;
-	
-	output = "";
+
 	if (fd < 0 || buff_size <= 0)
 		return (NULL);
-	curr = list_buff;
 	while(!has_newline(content))
 	{
 		read_until = read(fd, content, buff_size);
 		content[read_until] = '\0';
-		list_buff = ft_lstnew(content);
-		printf("%s", list_buff->str);
-		ft_lstadd_back(&curr, ft_lstnew(content));
-		curr = curr->next;
+		output = ft_strjoin(output, content, buff_size);
 	}
+	printf("%s", output);
 	
 	return (output);
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,67 +6,9 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:42:39 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/02 10:51:47 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/03 10:52:23 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	ft_lstsize(my_list *lst)
-{
-	int		count;
-	my_list	*current;
-
-	count = 0;
-	current = lst;
-	while (current != NULL)
-	{
-		count++;
-		current = current->next;
-	}
-	return (count);
-}
-
-my_list	*ft_lstlast(my_list *lst)
-{
-	my_list	*curr;
-	int		last_lst;
-
-	if (lst == NULL)
-		return (NULL);
-	last_lst = ft_lstsize(lst) - 1;
-	curr = lst;
-	while (curr != NULL && last_lst)
-	{
-		curr = curr->next;
-		last_lst--;
-	}
-	return (curr);
-}
-
-void	ft_lstadd_back(my_list **lst, my_list *new)
-{
-	my_list	*last_node;
-
-	if (!lst || !new)
-		return ;
-	if (*lst == NULL)
-		*lst = new;
-	else
-	{
-		last_node = ft_lstlast(*lst);
-		last_node->next = new;
-	}
-}
-
-my_list	*ft_lstnew(void *content)
-{
-	my_list	*ret_lst;
-
-	ret_lst = (my_list *)malloc(sizeof(my_list));
-	if (ret_lst == NULL)
-		return (NULL);
-	ret_lst->str = content;
-	ret_lst->next = NULL;
-	return (ret_lst);
-}
