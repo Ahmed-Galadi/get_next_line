05e3568be38d28be87338cff31e503206712ad39
Ahmed-Galadi <galadiahmed99@gmail.com>
2023-12-04 17:53:26 +0100

    separate the functions into the other file

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,14 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:47:22 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/04 17:25:41 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/04 17:41:32 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-int has_newline(char *str)
-{
-	int		i;
 
-	i = 0;
-	if (!str)
-		return (0);
-	while (str[i])
-	{
-		if(str[i] == '\n')
-			return (1);
-		i++;
-	}
-	return (0);
-}
 
 int ft_strlen(char *str)
 {
@@ -64,31 +50,6 @@ char *ft_strjoin(char *s1, char *s2)
 	}
 	joined[i] = '\0';
 	return (joined);
-}
-
-char *till_nl(char *content)
-{
-	char *output;
-	int i;
-
-	i = 0;
-	if (!content)
-		return (NULL);
-	output = (char *)malloc(ft_strlen(content) + 1);
-	if (!output)
-		return (NULL);
-	while (content[i])
-	{
-		if (content[i] == '\n')
-		{
-			output[i] = content[i];
-			break;
-		}
-		output[i] = content[i];
-		i++;
-	}
-	output[i + 1] = '\0';
-	return (output);
 }
 
 char *shyata_to_static(char *content)
@@ -138,7 +99,6 @@ char *get_next_line(int fd)
 		holder = ft_strjoin(holder, content);
 	}
 	shyata = shyata_to_static(holder);
-	printf("%s", shyata);
 	free(holder);
 	return (till_nl(holder));
 }
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,8 +6,49 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:42:39 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/03 23:13:56 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/04 17:41:27 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+int has_newline(char *str)
+{
+	int		i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i])
+	{
+		if(str[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char *till_nl(char *content)
+{
+	char *output;
+	int i;
+
+	i = 0;
+	if (!content)
+		return (NULL);
+	output = (char *)malloc(ft_strlen(content) + 1);
+	if (!output)
+		return (NULL);
+	while (content[i])
+	{
+		if (content[i] == '\n')
+		{
+			output[i] = content[i];
+			break;
+		}
+		output[i] = content[i];
+		i++;
+	}
+	output[i + 1] = '\0';
+	return (output);
+}
