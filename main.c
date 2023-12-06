f8748a83148cc9925898e48d90cd4962f17f820d
Ahmed-Galadi <galadiahmed99@gmail.com>
2023-12-06 23:50:52 +0100

    check leeks

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:47:31 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/05 17:28:37 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/06 23:50:26 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ int main()
 	char *str4 = get_next_line(fd);
 	char *str5 = get_next_line(fd);
 	printf("%s%s%s%s%s%s", str, str1, str2, str3, str4, str5);
+	system(leeks a.out);
 	close(fd);
 	return (0);
 }
