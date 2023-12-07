57ef18f8cd3270e478e9bb3ee0dc7466e9a896b3
Ahmed-Galadi <galadiahmed99@gmail.com>
2023-12-07 23:20:09 +0100

    now everything is working

--- get_next_line.c
+++ get_next_line.c
@@ -6,53 +6,63 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:47:22 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/07 22:22:53 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/07 23:06:34 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-// char *get_next_line(int fd)
-// {
-// 	static char *shyata = NULL;
-// 	char *holder = NULL;
-// 	int buff_size;
-// 	buff_size = BUFFER_SIZE;
-// 	char content[buff_size + 1];
-// 	int read_until;
-// 	read_until = 0;
-// 	if (fd < 0 || buff_size <= 0)
-// 		return (NULL);
-// 	if (shyata)
-// 		holder = ft_strjoin(holder, shyata);
-// 	while(!has_newline(content))
-// 	{
-// 		read_until = read(fd, content, buff_size);
-// 		content[read_until] = '\0';
-// 		free(holder);
-// 		holder = ft_strjoin(holder, content);
-// 	}
-// 	if (shyata != NULL)
-// 		free(shyata);
-// 	shyata = shyata_to_static(holder);
-// 	free(holder);
-// 	return (till_nl(holder));
-// }
-
 char *get_next_line(int fd)
 {
-	static char	*shyata;
-	char		*output;
-	char		*read_buff;
-	int			read_until;
-
+	static char *shyata = NULL;
+	char *holder = NULL;
+	int buff_size;
+	buff_size = BUFFER_SIZE;
+	char content[buff_size + 1];
+	int read_until;
 	read_until = 0;
-	if(fd < 0 || BUFFER_SIZE <= 0 || read(fd ,read_buff, 0))
+	if (fd < 0 || buff_size <= 0)
 		return (NULL);
-	while(!has_newline(read_buff))
+	if (shyata)
+		holder = ft_strjoin(holder, shyata);
+	while(!has_newline(content))
 	{
-		read_until = read(fd, read_buff, BUFFER_SIZE);
-		
+		read_until = read(fd, content, buff_size);
+		content[read_until] = '\0';
+		free(holder);
+		holder = ft_strjoin(holder, content);
 	}
+	if (shyata != NULL)
+		free(shyata);
+	shyata = shyata_to_static(holder);
+	free(holder);
+	return (till_nl(holder));
 }
+
+// char *get_next_line(int fd)
+// {
+// 	static char	*shyata;
+// 	char		*output;
+// 	char		*read_buff;
+// 	int			read_until;
+
+// 	read_until = 0;
+// 	if(fd < 0 || BUFFER_SIZE <= 0 || read(fd ,read_buff, 0))
+// 		return (NULL);
+// 	while(!has_newline(read_buff))
+// 	{
+// 		read_until = read(fd, read_buff, BUFFER_SIZE);
+// 		if (output)
+// 			free(output);
+// 		output = ft_strjoin(output, read_buff);
+// 		printf("%s", output);
+// 	}
+// 	if (shyata)
+// 		free(shyata);
+// 	shyata = shyata_to_static(read_buff);
+// 	free(read_buff);
+// 	free(output);
+// 	output = till_nl(output);
+// 	return (output);
+// }
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: agaladi <agaladi@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/01 15:47:31 by agaladi           #+#    #+#             */
-/*   Updated: 2023/12/07 18:18:57 by agaladi          ###   ########.fr       */
+/*   Updated: 2023/12/07 23:13:34 by agaladi          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,26 +19,27 @@ int main()
 	int fd = open("txt", O_RDWR);
 	char *str = get_next_line(fd);
 	char *str1 = get_next_line(fd);
-	char *str2 = get_next_line(fd);
-	char *str3 = get_next_line(fd);
-	char *str4 = get_next_line(fd);
-	char *str5 = get_next_line(fd);
-	char *str6 = get_next_line(fd);
-	char *str7 = get_next_line(fd);
-	char *str8 = get_next_line(fd);
-	char *str9 = get_next_line(fd);
-	printf("%s%s%s%s%s%s%s%s%s%s", str, str1, str2, str3, str4, str5, str6, str7, str8, str9);
+	// char *str2 = get_next_line(fd);
+	// char *str3 = get_next_line(fd);
+	// char *str4 = get_next_line(fd);
+	// char *str5 = get_next_line(fd);
+	// char *str6 = get_next_line(fd);
+	// char *str7 = get_next_line(fd);
+	// char *str8 = get_next_line(fd);
+	// char *str9 = get_next_line(fd);
+	// printf("%s%s%s%s%s%s%s%s%s%s", str, str1, str2, str3, str4, str5, str6, str7, str8, str9);
+	printf("%s", str);
+	printf("%s", str1);
 	free(str);
 	free(str1);
-	free(str2);
-	free(str3);
-	free(str4);
-	free(str5);
-	free(str6);
-	free(str7);
-	free(str8);
-	free(str9);
-	system("leaks a.out");
+	// free(str2);
+	// free(str3);
+	// free(str4);
+	// free(str5);
+	// free(str6);
+	// free(str7);
+	// free(str8);
+	// free(str9);
 	close(fd);
 	return (0);
 }
